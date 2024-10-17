9ebd74ac9c074ae8c8e0850b363ba30e472cf03b
liammtech <liammtech@gmail.com>
2024-10-17 21:12:18 +0100

    Create /projects endpoint calls in winnerService.js

--- services/winnerService.js
+++ services/winnerService.js
@@ -2,6 +2,7 @@ const axios = require('axios');
 const config = require('../config/config')
 
 // API Interface
+
 const winnerApi = axios.create({
     baseUrl: config.winner.apiUrl,
     headers: {
@@ -11,7 +12,29 @@ const winnerApi = axios.create({
 });
 
 // Projects
+
 const filterProjects = async (projectNumberList) => {
     const response = await winnerApi.post('/projects/filter', { projectNumberList });
+    return response.data;
+};
+
+const getProject = async (projectGuid, shopGuid) => {
+    const response = await winnerApi.get(`/projects/${projectGuid}`, {
+        params: { shopGuid }
+    });
+    return response.data;
+}
+
+const updateProject = async (projectGuid, shopGuid, projectData) => {
+    const response = await winnerApi.put(`/projects/${projectGuid}`, projectData, {
+        params: { shopGuid }
+    });
+    return response.data;
+};
+
+const createProject = async (projectGuid, shopGuid, projectData) => {
+    const response = await winnerApi.post(`/projects`, projectData, {
+        params: { shopGuid }
+    });
     return response.data;
 };
